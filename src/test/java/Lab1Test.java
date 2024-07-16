0f1b99e93236598da47d96f6687afcea199426c9
RevvingUpThatHill <theodore.balashov@revature.com>
2024-07-16 13:31:32 -0400

    refactor for language agnostic

--- src/test/java/Lab1Test.java
+++ src/test/java/Lab1Test.java
@@ -1,5 +1,6 @@
 
 import Util.ConnectionUtil;
+import Util.FileUtil;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -8,27 +9,32 @@ import org.junit.Test;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 
-public class AlterATableTest {
-    AlterATable alterATable = new AlterATable();
+public class Lab1Test {
 
 
     @Test
     public void problem1Test(){
-        alterATable.problem1();
 
         try {
-
+            String sql = FileUtil.parseSQLFile("src/main/lab1.sql");
+            if(sql.isEmpty()){
+                Assert.fail("No SQL statement in lab1.sql.");
+            }
             Connection connection = ConnectionUtil.getConnection();
-            String sql = "select lastname from site_user;";
-
-            PreparedStatement ps = connection.prepareStatement(sql);
-
-            ps.executeQuery();
-
+            Statement s = connection.createStatement();
+            s.executeUpdate(sql);
         } catch (SQLException e) {
-            System.out.println("problem1: " + e.getMessage() + '\n');
-            Assert.fail();
+            Assert.fail("There was an issue running your sql statement in lab1.sql: "+e.getMessage());
+        }
+        try {
+            Connection connection = ConnectionUtil.getConnection();
+            String sql = "select lastname from Person;";
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.executeQuery();
+        } catch (SQLException e) {
+            Assert.fail("Could not select the lastname column from the Person table.");
         }
     }
     /**
@@ -41,10 +47,10 @@ public class AlterATableTest {
             Connection connection = ConnectionUtil.getConnection();
 
             //Write SQL logic here
-            String sql1 = "CREATE TABLE site_user (id SERIAL PRIMARY KEY, firstname varchar(100));";
-            String sql2 = "INSERT INTO site_user (firstname) VALUES ('Kevin');";
-            String sql3 = "INSERT INTO site_user (firstname) VALUES ('Brian');";
-            String sql4 = "INSERT INTO site_user (firstname) VALUES ('Charles');";
+            String sql1 = "CREATE TABLE Person (id SERIAL PRIMARY KEY, firstname varchar(100));";
+            String sql2 = "INSERT INTO Person (firstname) VALUES ('Kevin');";
+            String sql3 = "INSERT INTO Person (firstname) VALUES ('Brian');";
+            String sql4 = "INSERT INTO Person (firstname) VALUES ('Charles');";
 
             PreparedStatement ps = connection.prepareStatement(sql1 + sql2 + sql3 + sql4);
 
@@ -63,7 +69,7 @@ public class AlterATableTest {
         try {
 
             Connection connection = ConnectionUtil.getConnection();
-            String sql = "DROP TABLE site_user;";
+            String sql = "DROP TABLE Person;";
 
             PreparedStatement ps = connection.prepareStatement(sql);
 
